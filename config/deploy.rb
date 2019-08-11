8cc1410c0404fbb48db6c9e340fc33cba231a00b
Stavri Thomollari <stavrithomollari@outlook.com>
2019-08-12 01:53:22 +0200

    vdf

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, "https://github.com/StavriThomollari/time_logger.git"
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
-
+set :linked_files, %w{config/master.key config/database.yml}
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
 
